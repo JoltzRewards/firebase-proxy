4fcf6c1e2f3312ba2bbf03779ca7237d60ab0354
Linden <70739041+linden@users.noreply.github.com>
2022-09-01 16:05:47 -0700

    feat(storage): add `uploadBytesResumable` & `getDownloadURL` support

--- packages/storage/src/index.js
+++ packages/storage/src/index.js
@@ -10,19 +10,48 @@ export function getStorage(app) {
 
 export function ref(storage, path) {
 	logger.log("finding file at path", { path });
+
+	return path;
 }
 
 class Upload {
 	constructor(reference, content) {
 		this.reference = reference;
 		this.content = content;
-		this.snapshot = { ref: "" };
+
+		this.snapshot = { ref: null };
 	}
 
 	on(event, snapshot, error, done) {
 		logger.log("file state watching", { event, snapshot, error, done });
 
-		done();
+		if (!window.nhost) {
+			logger.log("failed to upload since nhost has not started", { reference: this.reference });
+
+			error("nhost has not started");
+
+			return;
+		}
+
+		window.nhost.storage.upload({ 
+			file: this.content,
+			name: this.reference
+		})
+		.then((result) => {
+			if (result.error) {
+				error(result.error);
+
+				return;
+			}
+
+			this.snapshot.ref = result.fileMetadata.id;
+
+			logger.log("uploaded with result", { reference: this.reference, result })
+
+			done();
+
+			return;
+		});		
 	}
 }
 
@@ -35,19 +64,17 @@ export function uploadBytesResumable(reference, content) {
 export async function getDownloadURL(reference) {
 	logger.log("get download URL", { reference });
 
-	return "https://www.example.com";
+	const publicURL = nhost.storage.getPublicUrl({ fileId: reference })
+
+	logger.log("got download URL", { reference, publicURL });
+
+	return publicURL;
 }
 
 export async function listAll(reference) {
 	logger.log("listing all files", { reference });
 
-	return {
-		items: [{
-			_location: {
-				path_: "example"
-			}
-		}]
-	}
+	return {}
 }
 
 export async function deleteObject(reference) {
