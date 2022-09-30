8e2904c71ef5cb038f09a8d0031208873e592f8c
Linden <70739041+linden@users.noreply.github.com>
2022-09-30 14:52:42 -0700

    fix(app): expect `configuration` to be `null` & set `Logger.source` to "app"

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -1,13 +1,13 @@
 import { NhostClient } from "@nhost/nhost-js";
 import { Logger } from "@trubittech/firebase-proxy-core";
 
-export const logger = new Logger("firestore");
+export const logger = new Logger("app");
 
 export function initializeApp(configuration) {
 	if (!window.nhost) {
 		const state = {
-			subdomain: configuration.domain || import.meta.env.VITE_NHOST_SUBDOMAIN || "localhost",
-			region: configuration.region  || import.meta.env.VITE_NHOST_REGION || null
+			subdomain: configuration?.domain || import.meta.env.VITE_NHOST_SUBDOMAIN || "localhost",
+			region: configuration?.region || import.meta.env.VITE_NHOST_REGION || null
 		}
 
 		logger.log("creating new app adapter", state);
